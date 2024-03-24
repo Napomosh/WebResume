6af844f8bfb3ef7affd2d9181d4152517cd2ec16
Меллер Александр <meller.alexander@csoft.ru>
2024-03-24 20:23:49 +0300

    Add dynamic menu bar

---
+++ WebResume/ViewComponents/MainMenuViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebResume.BL.Auth;
+
+namespace WebResume.ViewComponents;
+
+public class MainMenuViewComponent(ICurrentUser currentUser) : ViewComponent{
+    public async Task<IViewComponentResult> InvokeAsync(){
+        bool isLoggedIn = await currentUser.IsLoggedIn();
+        return View("Index", isLoggedIn);
+    }
+}
